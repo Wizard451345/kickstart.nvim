1a79a5405b16811604715a26b001936a7538c582
Wizard451345 <wiz451345.my@gmail.com>
2024-01-26 18:15:32 -0600

    Added Kickstart folder (I forgot anyways), DAP and formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,19 +7,19 @@ return {
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
 
   -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  "tpope/vim-sleuth",
 
-  -- NOTE: Personal STARTS HERE! sync with GIT please! :) 
+  -- NOTE: Personal STARTS HERE! sync with GIT please! :)
 
-  'norcalli/nvim-colorizer.lua', --Note, TOGGLED OFF
-  'andweeb/presence.nvim',
+  "norcalli/nvim-colorizer.lua", --Note, TOGGLED OFF
+  "andweeb/presence.nvim",
 
   {
-    'folke/todo-comments.nvim',
+    "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       -- your configuration comes here
@@ -28,49 +28,61 @@ return {
     },
   },
 
-  -- See :h autoclose.nvim-* 
+  -- See :h autoclose.nvim-*
   {
-    'm4xshen/autoclose.nvim',
-    config = function ()
+    "m4xshen/autoclose.nvim",
+    config = function()
       require("autoclose").setup({
         keys = {
           ["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
         },
       })
-    end
+    end,
   },
 
   --see :h bufferline-configuration
   --TODO: buffer or tab?
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function ()
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
       require("bufferline").setup({
         options = {
           mode = "buffers",
           numbers = "ordinal",
-        }
+        },
       })
-    end
+    end,
   },
 
   {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
     config = function()
-      require('dashboard').setup ({
+      require("dashboard").setup({
         theme = "hyper",
         shortcut_type = "number",
         config = {
           shortcut = {
 
             --Used my personal directory,$USER did not work. -- Compare GIT before changing
-            {desc = "Files [WIN]", group = "Label", action = "Telescope find_files cwd=D:\\", key = "f"},
-            {desc = "Config [WIN]", action = "e C:\\Users\\marco\\AppData\\Local\\nvim\\init.lua", key = "e"},
-            {desc = "Config [LIN]", action = "e ~/config/nvim/init.lua", key = "E"},
+            {
+              desc = "Files [WIN]",
+              group = "Label",
+              action = "Telescope find_files cwd=D:\\",
+              key = "f",
+            },
+            {
+              desc = "Config [WIN]",
+              action = "e C:\\Users\\marco\\AppData\\Local\\nvim\\init.lua",
+              key = "e",
+            },
+            { desc = "Config [LIN]", action = "e ~/config/nvim/init.lua", key = "E" },
           },
           project = {
             enable = "True",
-            action = "Telescope find_files cwd="
+            action = "Telescope find_files cwd=",
           },
           week_header = {
             enable = "true",
@@ -78,7 +90,7 @@ return {
         },
       })
     end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
 
   {
@@ -89,7 +101,11 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup({
+        view = {
+          side = "right", -- I like it to show on right. Like a boss
+        },
+      })
     end,
   },
   -- Personal configuration ends here
@@ -98,49 +114,49 @@ return {
   --  The configuration is done below. Search for lspconfig to find it below.
   {
     -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { "j-hui/fidget.nvim",       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+      "folke/neodev.nvim",
     },
   },
 
-  { -- Autocompletion, UPDATED from fork Jan 19 smth
-    'hrsh7th/nvim-cmp', --epic auto complete tab
+  {                     -- Autocompletion, UPDATED from fork Jan 19 smth
+    "hrsh7th/nvim-cmp", --epic auto complete tab
     dependencies = {
       -- Snippet Engine & its associtaed nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
 
       -- Adds LSP Stuff!
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
 
       -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      "rafamadriz/friendly-snippets",
     },
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { "folke/which-key.nvim", opts = {} },
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes. APPEARS AT LUALINE
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
       },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
@@ -152,119 +168,123 @@ return {
         end
 
         -- Navigation
-        map({ 'n', 'v' }, ']c', function()
+        map({ "n", "v" }, "]c", function()
           if vim.wo.diff then
-            return ']c'
+            return "]c"
           end
           vim.schedule(function()
             gs.next_hunk()
           end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to next hunk' })
+          return "<Ignore>"
+        end, { expr = true, desc = "Jump to next hunk" })
 
-        map({ 'n', 'v' }, '[c', function()
+        map({ "n", "v" }, "[c", function()
           if vim.wo.diff then
-            return '[c'
+            return "[c"
           end
           vim.schedule(function()
             gs.prev_hunk()
           end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to previous hunk' })
+          return "<Ignore>"
+        end, { expr = true, desc = "Jump to previous hunk" })
 
         -- Actions
         -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
+        map("v", "<leader>hs", function()
+          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "stage git hunk" })
+        map("v", "<leader>hr", function()
+          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "reset git hunk" })
         -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
+        map("n", "<leader>hS", gs.stage_buffer, { desc = "git Stage buffer" })
+        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
+        map("n", "<leader>hR", gs.reset_buffer, { desc = "git Reset buffer" })
+        map("n", "<leader>hp", gs.preview_hunk, { desc = "preview git hunk" })
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = false })
+        end, { desc = "git blame line" })
+        map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
+        map("n", "<leader>hD", function()
+          gs.diffthis("~")
+        end, { desc = "git diff against last commit" })
 
         -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+        map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
+        map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
 
         -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select git hunk" })
       end,
     },
   },
 
-
   { -- Theme inspired by Atom.
-    'navarasu/onedark.nvim',
+    "navarasu/onedark.nvim",
     priority = 1000,
-    config = function() -- Makes it more stable. If installed, run function, if not, Lazy install colorscheme, then run config.
-      vim.cmd.colorscheme 'onedark' --NOTE: autorun this command 
+    config = function()              -- Makes it more stable. If installed, run function, if not, Lazy install colorscheme, then run config.
+      vim.cmd.colorscheme("onedark") --NOTE: autorun this command
     end,
   },
 
   {
-    'sainnhe/everforest',
+    "sainnhe/everforest",
     priority = 500,
-    config = function ()
-      vim.cmd.colorscheme 'everforest'
+    config = function()
+      vim.cmd.colorscheme("everforest")
     end,
   },
 
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { "catppuccin/nvim",      name = "catppuccin", priority = 1000 },
 
   { -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     -- See `:help lualine.txt`
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
+        theme = "onedark",
+        component_separators = "|",
+        section_separators = "",
       },
     },
   },
 
   {
     -- Add indentation guides even on blank lines -- WARNING: Works ONLY on the newest NEOVIM VERSION
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
-    main = 'ibl',
+    main = "ibl",
     opts = {},
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { "numToStr/Comment.nvim", opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
+        "nvim-telescope/telescope-fzf-native.nvim",
         -- NOTE: If you are having trouble with this installation,
         --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
+
+        -- build = "make -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        build = "make",
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable("make") == 1
         end,
       },
     },
@@ -272,10 +292,10 @@ return {
 
   {
     -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    build= 'TSUpdate',
-  }
+    build = "TSUpdate",
+  },
 }
