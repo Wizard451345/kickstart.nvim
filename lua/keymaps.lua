2db8d9c353c4a20177413ed151054f98cda6a605
Wizard451345 <wiz451345.my@gmail.com>
2024-05-06 17:26:26 -0500

    fix dumb mistake

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,7 @@
 -- See `:help vim.keymap.set()`
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.keymap.set({ "n", "<Esc>" }, "<cmd>nohlsearch<CR>")
+vim.keymap.set( "n", "<Esc>" , "<cmd>nohlsearch<CR>")
 
 -- Remap for dealing with word wrap
 -- vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
