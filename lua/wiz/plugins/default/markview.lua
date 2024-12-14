bc080d5bfc7e761ea4576da8ae851a72258c4f6a
Wizard451345 <wiz451345.my@gmail.com>
2024-12-13 22:36:31 -0600

    Markview minor changes

--- lua/wiz/plugins/default/markview.lua
+++ lua/wiz/plugins/default/markview.lua
@@ -1,28 +1,28 @@
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  ft = "markdown",
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	ft = { "markdown", "md", "vimwiki", "wiki", "txt", "text", "rmd", "rmarkdown" },
 
-  dependencies = {
-    -- You may not need this if you don't lazy load
-    -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		-- You may not need this if you don't lazy load
+		-- Or if the parsers are in your $RUNTIMEPATH
+		"nvim-treesitter/nvim-treesitter",
 
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("markview").setup({
-      modes = { "n", "i", "no", "c" },
-      hybrid_modes = { "i" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("markview").setup({
+			modes = { "n", "i", "no", "c" },
+			hybrid_modes = { "i" },
 
-      -- This is nice to have
-      callbacks = {
-        on_enable = function(_, win)
-          vim.wo[win].conceallevel = 2
-          vim.wo[win].concealcursor = "nc"
-        end,
-      },
-    })
-  end,
+			-- This is nice to have
+			callbacks = {
+				on_enable = function(_, win)
+					vim.wo[win].conceallevel = 2
+					vim.wo[win].concealcursor = "nc"
+				end,
+			},
+		})
+	end,
 }
 -- vim: ts=2 sts=2 sw=2 et
